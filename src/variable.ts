a0dee70ac686aa397cfcbcdfd3ce0e99b09b210d
Ha0 <hayeong28@naver.com>
2020-07-05 15:44:13 +0900

    let 키워드 : 블럭단위 변수선언 키워드

--- src/variable.ts
+++ src/variable.ts
@@ -3,9 +3,11 @@ let score2 = 200;
 const defaultScore = 0;
 
 function outer() {
-  var score = 0;
+  if (true) {
+    let score = 0;
+  }
 
-  for (var i = 0; i < 3; i++) {
+  for (let i = 0; i < 3; i++) {
     setTimeout(function () {
       console.log(i);
     }, 100);
