fddc9562b114c67eb3e03d28a08fd0c573d8e0ee
Ha0 <hayeong28@naver.com>
2020-07-05 15:22:36 +0900

    learn tsc - learn tsc config

--- dist/hello.js
+++ dist/hello.js
@@ -1,13 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var hello = "hello";
-let hello2 = "hello2";
-let timeoutPromise = new Promise((resolve, reject) => {
-    setTimeout(() => {
+var hello2 = "hello2";
+var timeoutPromise = new Promise(function (resolve, reject) {
+    setTimeout(function () {
         resolve("1 sec");
     }, 1000);
 });
 timeoutPromise.then(console.log);
-const utils_1 = require("./utils");
-const returnVal = utils_1.default(1, 2);
+var utils_1 = require("./utils");
+var returnVal = utils_1.default(1, 2);
 console.log(returnVal);
